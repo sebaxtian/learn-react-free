ed8119dfd18c703987849ae300c9aec37d9c68a5
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-03-03 11:25:37 -0500

    First React Parent/Child Components Hello World!

---
+++ setup-from-scratch/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <nav>
+                <h1>Hello World!</h1>
+                <ul>
+                    <li>Thing 1</li>
+                    <li>Thing 2</li>
+                    <li>Thing 3</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
